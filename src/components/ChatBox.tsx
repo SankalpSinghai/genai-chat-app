96fef2a0f6ca9754361a8a5a9394e623083afe6b
Sankalp Singhai <sankalpsinghai@Sankalps-MacBook-Air.local>
2026-02-02 18:53:48 +0530

    feat: Add stream functionality to chat UI

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,82 +1,106 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
 
 type Message = {
-    role: 'user' | 'assistant';
-    content: string;
-}
+  role: "user" | "assistant";
+  content: string;
+};
 
 export const ChatBot = () => {
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [input, setInput] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
 
-    async function sendMessage() {
-        if (!input.trim()) return;
+  async function sendMessage() {
+    if (!input.trim()) return;
 
-        const userMessage: Message = {
-            role: 'user',
-            content: input,
-        }
+    const userMessage: Message = {
+      role: "user",
+      content: input,
+    };
 
-        setMessages((prev) => [...prev, userMessage]);
-        setInput('');
-        setLoading(true);
+    setMessages((prev) => [...prev, userMessage]);
+    setInput("");
+    setLoading(true);
 
-        try {
-            const res = await fetch(`api/chat`, {
-                method: "POST",
-                headers: {
-                    'CONTENT-TYPE': "application/json",
-                },
-                body: JSON.stringify({ message: userMessage.content })
-            });
+    try {
+      const res = await fetch(`/api/chat`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: userMessage.content }),
+      });
 
-            const data = await res.json();
+      const reader = res.body?.getReader();
+      const decoder = new TextDecoder();
 
-            const aiMessage: Message = {
-                role: 'assistant',
-                content: data.reply,
-            }
+      let aiText = "";
 
-            setMessages((prev) => [...prev, aiMessage]);
-        } catch (error) {
-            console.log('Error occured', error);
-        } finally {
-            setLoading(false);
-        }
+      setMessages((prev) => [...prev, { role: "assistant", content: "" }]);
+
+      while (true) {
+        const { value, done } = await reader!.read();
+        if (done) break;
+
+        const chunk = decoder.decode(value);
+        aiText += chunk;
+
+        setMessages((prev) => {
+          const updated = [...prev];
+
+          updated[updated.length - 1] = {
+            role: "assistant",
+            content: aiText,
+          };
+          return updated;
+        });
+      }
+    } catch (error) {
+      console.log("Error occured", error);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return (
-        <div className="max-w-xl mx-auto p-4">
-            <h1 className="text-xl mb-4 font-semibold">Gen AI Chat</h1>
-            <div>
-                {messages.map((message, idx) => (
-                    <div
-                        key={idx}
-                        className={`p-2 rounded ${message.role === 'user' ? "bg-blue-100 text-right" : "bg-gray-50 text-left max-w-80"
-                            }`}
-                    >
-                        {message.content}
-                        {loading && <div className="text-sm text-gray-500">AI is thinking...</div>}
-                    </div>
-                ))}
-            </div>
-            <div className="flex gap-2">
-                <input
-                    value={input}
-                    className="flex-1 border rounded p-2"
-                    onChange={(e) => { setInput(e.target.value) }}
-                    placeholder="Ask something..."
-                />
-                <button
-                    onClick={sendMessage}
-                    className="bg-black text-white px-4 rounded"
-                >
-                    Send
-                </button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="max-w-xl mx-auto p-4">
+      <h1 className="text-xl mb-4 font-semibold">Gen AI Chat</h1>
+      <div>
+        {messages.map((message, idx) => (
+          <div
+            key={idx}
+            className={`p-2 rounded ${
+              message.role === "user"
+                ? "bg-blue-100 text-right"
+                : "bg-gray-50 text-left max-w-80"
+            }`}
+          >
+            {message.content}
+          </div>
+        ))}
+        {loading && (
+          <div className="text-sm text-gray-500">AI is thinking...</div>
+        )}
+      </div>
+      <div className="flex gap-2">
+        <input
+          value={input}
+          className="flex-1 border rounded p-2"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          placeholder="Ask something..."
+        />
+        <button
+          onClick={sendMessage}
+          className="bg-black text-white px-4 rounded"
+          disabled={loading}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
